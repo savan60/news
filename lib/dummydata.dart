9b6279775a9cb716d2ee69cf1bcff3d72f950901
savan60 <51092808+savan60@users.noreply.github.com>
2021-05-16 01:27:26 +0530

    video page done

---
+++ lib/dummydata.dart
@@ -0,0 +1,53 @@
+import 'package:news/model/news.dart';
+
+const DUMMY_NEWS = const [
+  News(
+    title: "This is Heading of the related new or may be extend",
+    date: "03-02-2021",
+    description: "This is Heading of the related new or may be extend,This is Heading of the related new or may be extend",
+    url: "assets/images/news1.jpg",
+  ),
+  News(
+    title: "This is Heading of the related new or may be extend",
+    date: "03-02-2021",
+    description: "This is Heading of the related new or may be extend",
+    url: "assets/images/news2.jpg",
+  ),
+  News(
+    title: "This is Heading of the related new or may be extend",
+    date: "03-02-2021",
+    description: "This is Heading of the related new or may be extend,This is Heading of the related new or may be extend",
+    url: "assets/images/news3.jpeg",
+  ),
+  News(
+    title: "This is Heading of the related new or may be extend",
+    date: "03-02-2021",
+    description: "This is Heading of the related new or may be extend,This is Heading of the related new or may be extend",
+    url: "assets/images/news4.jpg",
+  ),
+  News(
+    title: "This is Heading of the related new or may be extend",
+    date: "03-02-2021",
+    description: "This is Heading of the related new or may be extend,This is Heading of the related new or may be extend",
+    url: "assets/images/news5.jpg",
+  ),
+  News(
+    title: "This is Heading of the related new or may be extend",
+    date: "03-02-2021",
+    description: "This is Heading of the related new or may be extend,This is Heading of the related new or may be extend",
+    url: "assets/images/news6.jpg",
+  ),
+  News(
+    title: "This is Heading of the related new or may be extend",
+    date: "03-02-2021",
+    description: "This is Heading of the related new or may be extend,This is Heading of the related new or may be extend",
+    url: "assets/images/news7.jpg",
+  ),
+  News(
+    title: "This is Heading of the related new or may be extend",
+    date: "03-02-2021",
+    description: "This is Heading of the related new or may be extend,This is Heading of the related new or may be extend",
+    url: "assets/images/news8.jpg",
+  ),
+  
+];
---
+++ lib/model/news.dart
@@ -0,0 +1,8 @@
+class News{
+  final String title;
+  final String url;
+  final String date;
+  final String description;
+
+  const News({this.title,this.date,this.description,this.url});
+}
